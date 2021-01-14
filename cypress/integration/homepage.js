a5ac8247f6185fc2c30fc7a19f001f11b422e3a2
tbnoung <tbnoung@gmail.com>
2021-01-15 00:28:37 +0700

    a

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,10 +1,8 @@
-import { Encode } from '../../services/index'
-
 describe('Check home page', () => {
   it('visits check home page',  () => {
-    cy.getData()
-    cy.visit('http://localhost:8080/seller')
-    cy.wait(1000)
-   cy.Createproduct()
+    // cy.getData()
+  //   cy.visit('http://localhost:8080/seller')
+  //   cy.wait(1000)
+  //   cy.Createproduct()
   })
 })
